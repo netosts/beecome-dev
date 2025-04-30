d6833862c576a42794a572b67d3b0da1d3c3dccc
netosts <netostt91@gmail.com>
2025-04-30 13:28:18 -0300

    add base url to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/beecome-dev/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
